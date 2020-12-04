d30ed02ae519b077eae5c1d2b1dd27b939e69f4c
Paul Uhn <paulu@moj.io>
2020-12-04 09:21:54 -0600

    Day 4 Part 2

--- aoc/2020/2020Day4.swift
+++ aoc/2020/2020Day4.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Y2020Day4 {
     
     struct Passport {
-        enum DataType: String {
+        enum Field: String {
             case birthYear  = "byr"
             case issueYear  = "iyr"
             case expireYear = "eyr"
@@ -21,10 +21,16 @@ struct Y2020Day4 {
             case passportID = "pid"
             case countryID  = "cid"
         }
-        let data: [DataType: String]
+        private let data: [Field: String]
+        var hasRequiredFields: Bool {
+            return data.keys
+                .filter { $0 != .countryID } // optional
+                .count == 7
+        }
         var isValid: Bool {
             return data.keys
                 .filter { $0 != .countryID } // optional
+                .filter { isValidData(for: $0) }
                 .count == 7
         }
         
@@ -32,12 +38,55 @@ struct Y2020Day4 {
             self.data = data
                 .flatMap { $0.split(separator: " ") }
                 .map { $0.split(separator: ":").map(String.init) }
-                .map { (DataType(rawValue: $0.first!)!, $0.last!) }
+                .map { (Field(rawValue: $0.first!)!, $0.last!) }
                 .reduce(into: [:], { $0[$1.0] = $1.1 })
+        }
+        private func isValidData(for field: Field) -> Bool {
+            switch field {
+            case .birthYear: return isValidRange(data[.birthYear], min: 1920, max: 2002)
+            case .issueYear: return isValidRange(data[.issueYear], min: 2010, max: 2020)
+            case .expireYear: return isValidRange(data[.expireYear], min: 2020, max: 2030)
+            case .height: return isValidHeight(data[.height])
+            case .hairColor: return isValidHair(data[.hairColor])
+            case .eyeColor: return isValidEye(data[.eyeColor])
+            case .passportID: return isValidIdentifier(data[.passportID])
+            case .countryID: return true
+            }
+        }
+        private func isValidRange(_ value: String?, min: Int, max: Int) -> Bool {
+            guard let value = value?.int else { return false }
+            return value >= min && value <= max
+        }
+        private func isValidHeight(_ height: String?) -> Bool {
+            guard let height = height else { return false }
+            let regex = try! NSRegularExpression(pattern: "^([0-9]+)(cm|in)$")
+            guard let match = regex.firstMatch(in: height, options: [], range: NSRange(location: 0, length: height.count)) else { return false }
+            switch height.match(match, at: 2) {
+            case "cm": return isValidRange(height.match(match, at: 1), min: 150, max: 193)
+            case "in": return isValidRange(height.match(match, at: 1), min: 59, max: 76)
+            default: return false
+            }
+        }
+        private func isValidHair(_ color: String?) -> Bool {
+            guard let color = color else { return false }
+            let regex = try! NSRegularExpression(pattern: "^#([0-9a-f]{6})$")
+            guard let _ = regex.firstMatch(in: color, options: [], range: NSRange(location: 0, length: color.count)) else { return false }
+            return true
+        }
+        private func isValidEye(_ color: String?) -> Bool {
+            guard let color = color else { return false }
+            let colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+            return colors.contains(color)
+        }
+        private func isValidIdentifier(_ id: String?) -> Bool {
+            guard let id = id else { return false }
+            let regex = try! NSRegularExpression(pattern: "^([0-9]{9})$")
+            guard let _ = regex.firstMatch(in: id, options: [], range: NSRange(location: 0, length: id.count)) else { return false }
+            return true
         }
     }
 
-    static func Part1(_ data: [String]) -> Int {
+    private static func passports(_ data: [String]) -> [Passport] {
         var passports = [Passport]()
         
         var cache = [String]()
@@ -49,10 +98,18 @@ struct Y2020Day4 {
                 cache += [line]
             }
         }
-
         return passports
-            .filter { $0.isValid }
+    }
+    
+    static func Part1(_ data: [String]) -> Int {
+        return passports(data)
+            .filter { $0.hasRequiredFields }
             .count
     }
 
+    static func Part2(_ data: [String]) -> Int {
+        return passports(data)
+            .filter { $0.isValid }
+            .count
+    }
 }
--- aocTests/aoc2020Tests.swift
+++ aocTests/aoc2020Tests.swift
@@ -61,12 +61,23 @@ class aoc2020Tests: XCTestCase {
         assert(answer == 1478615040)
     }
     func testDay4Part1() {
-        let testData1 = testDataDay4().newlineSplit()
+        let testData1 = testDataDay4a().newlineSplit()
         assert(Y2020Day4.Part1(testData1) == 2)
         
         let answer = Y2020Day4.Part1(readInput(year: year, day: 4))
         print("\(#function):\(answer)")
         assert(answer == 226)
+    }
+    func testDay4Part2() {
+        let testData1 = testDataDay4b().newlineSplit()
+        assert(Y2020Day4.Part2(testData1) == 0)
+        
+        let testData2 = testDataDay4c().newlineSplit()
+        assert(Y2020Day4.Part2(testData2) == 4)
+        
+        let answer = Y2020Day4.Part2(readInput(year: year, day: 4))
+        print("\(#function):\(answer)")
+        assert(answer == 160)
     }
 }
 
@@ -103,7 +114,7 @@ extension aoc2020Tests {
 .#..#...#.#
 """
     }
-    func testDataDay4() -> String {
+    func testDataDay4a() -> String {
         return """
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -118,6 +129,41 @@ hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
+
+"""
+    }
+    func testDataDay4b() -> String {
+        return """
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+"""
+    }
+    func testDataDay4c() -> String {
+        return """
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
 
 """
     }
